afe4d6f9df465fc5f072a5b13a4b16b59fee0f13
Alen Grbic <alengrbic90@gmail.com>
2020-06-02 14:44:52 +0100

    Updates

---
+++ precache-manifest.fda0a25c1889c729fe6d9c2316bcc681.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89831d724229d0acfb6cee6eb3e1d555",
+    "url": "/covid-19-stats/index.html"
+  },
+  {
+    "revision": "29f58d1ed53f2ee5eea9",
+    "url": "/covid-19-stats/static/css/2.23fa0d6e.chunk.css"
+  },
+  {
+    "revision": "d73ed96b2c3c3fa4e0b9",
+    "url": "/covid-19-stats/static/css/main.4c8e2db4.chunk.css"
+  },
+  {
+    "revision": "29f58d1ed53f2ee5eea9",
+    "url": "/covid-19-stats/static/js/2.54f86495.chunk.js"
+  },
+  {
+    "revision": "f3a464b094a42227540b3ef9a73a0401",
+    "url": "/covid-19-stats/static/js/2.54f86495.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d73ed96b2c3c3fa4e0b9",
+    "url": "/covid-19-stats/static/js/main.a99f3707.chunk.js"
+  },
+  {
+    "revision": "31d7a03a6c18fcfb75fd",
+    "url": "/covid-19-stats/static/js/runtime-main.97c47e9e.js"
+  }
+]);
